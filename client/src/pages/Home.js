350827821187a1daa3839616843b00cf6d8f4c1d
Striddy2 <striddy2@yahoo.com>
2023-12-13 18:16:51 -0600

    adding row of circular photos

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,7 @@
 import './Home.css';
+import circle1 from "./photos/Circle photo 1.jpg";
+import circle2 from "./photos/Circle photo 2.jpg";
+import circle3 from "./photos/Circle photo 3.jpg";
 
 function Home() {
   return (
@@ -11,6 +14,11 @@ function Home() {
         </header>
       </div>
       <body className='second-segment'>
+        <div class="image-row">
+          <img src={circle1} alt=""></img>
+          <img src={circle2} alt=""></img>
+          <img src={circle3} alt=""></img>
+        </div>
         <h2>Shortly About Me</h2>0
       </body>
     </div>
